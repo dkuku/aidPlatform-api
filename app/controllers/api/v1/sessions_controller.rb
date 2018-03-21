a03046b240af33f537587288d1390f346302390c
Daniel Kukula <daniel.kuku@gmail.com>
2018-03-21 16:01:37 +0000

    logout route

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -1,7 +1,8 @@
 class Api::V1::SessionsController < Devise::SessionsController
   before_action :sign_in_params, only: :create
   before_action :load_user, only: :create
-
+  before_action :valid_token, only: :destroy
+  skip_before_action :verify_signed_out_user, only: :destroy
   #sign in
   def create
     if @user.valid_password?(sign_in_params[:password])
@@ -12,11 +13,16 @@ class Api::V1::SessionsController < Devise::SessionsController
     end
   end
 
+  def destroy
+	sign_out @user
+	json_response "Log out successfully", true, {}, :ok
+  end
+
   private
   def sign_in_params
     params.require(:sign_in).permit(:email, :password)
   end
-  
+
   def load_user
     @user = User.find_for_database_authentication(email: sign_in_params[:email])
     if @user
@@ -24,5 +30,14 @@ class Api::V1::SessionsController < Devise::SessionsController
     else
       json_response "Cannot get User", false, {}, :failure
     end
+  end
+
+  def valid_token
+	@user = User.find_by authentication_token: request.headers["AUTH-TOKEN"]
+	if @user
+	  return @user
+	else
+	  json_response "Invalid Token", false, {}, failure
+	end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,9 @@ Rails.application.routes.draw do
   namespace :api, defaults: {format: :json} do
     namespace :v1 do
       devise_scope :user do
-        post 'sign_up', to: 'registrations#create'
-        post 'sign_in', to: 'sessions#create'
+        post 	'sign_up', to: 'registrations#create'
+        post 	'sign_in', to: 'sessions#create'
+		delete 	'log_out', to: 'sessions#destroy'
       end
     end
   end
