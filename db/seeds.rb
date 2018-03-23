a03cfb5198062935dc5692983d9246df46e8d996
Daniel Kukula <daniel.kuku@gmail.com>
2018-03-23 05:23:57 +0000

    updated seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,23 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create! email: "dan@wp.pl",
+    password: "123qwe",
+    first_name: "daniel",
+    last_name: "kukula"
+
+9.times do |n|
+    User.create! email: Faker::Internet.email,
+        password: "123qwe",
+        first_name: Faker::Name.first_name,
+        last_name: Faker::Name.last_name
+end
+
+20.times do |n|
+    Task.create! title: Faker::Book.title,
+        description: Faker::Lorem.paragraph(5, true, 5),
+        lat: Faker::Address.latitude,
+        lng: Faker::Address.longitude,    
+        user_id: Faker::Number.between(1,10)
+end
