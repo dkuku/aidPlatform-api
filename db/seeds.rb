e0288895bde400f22ac85b4868976f27baa9512d
dkuku <daniel.kuku@gmail.com>
2018-03-25 17:21:13 +0100

    seeding more markers, solution for cors problem

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,17 +11,17 @@ User.create! email: "dan@wp.pl",
     first_name: "daniel",
     last_name: "kukula"
 
-9.times do |n|
+29.times do |n|
     User.create! email: Faker::Internet.email,
         password: "123qwe",
         first_name: Faker::Name.first_name,
         last_name: Faker::Name.last_name
 end
 
-20.times do |n|
+80.times do |n|
     Task.create! title: Faker::Book.title,
         description: Faker::Lorem.paragraph(5, true, 5),
-        lat: Faker::Address.latitude,
-        lng: Faker::Address.longitude,    
+        lat: Faker::Number.normal(51.5, 0.1),
+        lng: Faker::Number.normal(-0.2, 0.2),
         user_id: Faker::Number.between(1,10)
 end
