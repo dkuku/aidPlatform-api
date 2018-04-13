c7530ada1745c02f172ccffd4abdbd9170b4470c
Daniel Kukula <daniel.kuku@gmail.com>
2018-04-13 02:09:40 +0100

    data validation for user and task

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,3 +1,30 @@
 class Task < ApplicationRecord
+  
+  allowed_types = ['material', 'help']
+  
   belongs_to :user
+  validates_presence_of :title, :description, :lat, :lng, :user_id, :task_type,
+  {  message: "missing parameter" }
+
+	validates :title, length: { in: 2...50, 
+  message: "title must be between 2 and 50 letters"  }
+  
+  validates :description, length: { maximum: 300,
+  message: "your description is too long ,maximum 300 letters"  }
+
+  validates_inclusion_of :task_type, { in: allowed_types, 
+  message: "we don't support this type of request" }
+
+  validates :lat, numericality: {
+    only_integer: false,
+    greater_than_or_equal_to: 50, 
+    less_than_or_equal_to: 52,
+    message: "we currently don't work in this neighborhood"}
+
+  validates :lng, numericality: {
+    only_integer: false,
+    greater_than_or_equal_to: -1, 
+    less_than_or_equal_to: 1,
+    message: "we currently doni't work in this neighborhood"}
+
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,21 +6,21 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-User.create! email: "dan@wp.pl",
-    password: "123qwe",
-    first_name: "daniel",
-    last_name: "kukula"
-
-29.times do |n|
-    User.create! email: Faker::Internet.email,
-        password: "123qwe",
-        first_name: Faker::Name.first_name,
-        last_name: Faker::Name.last_name
-end
+#User.create! email: "dan@wp.pl",
+#    password: "123qwe",
+#    first_name: "daniel",
+#    last_name: "kukula"
+#
+#29.times do |n|
+#    User.create! email: Faker::Internet.email,
+#        password: "123qwe",
+#        first_name: Faker::Name.first_name,
+#        last_name: Faker::Name.last_name
+#end
 
 80.times do |n|
     Task.create! title: Faker::Book.title,
-        description: Faker::Lorem.paragraph(5, true, 5),
+        description: Faker::Lorem.paragraph(1, true, 4),
         lat: Faker::Number.normal(51.5, 0.1),
         lng: Faker::Number.normal(-0.2, 0.2),
         user_id: Faker::Number.between(1,10),
