768c28bad9f45707faf2b95008b070310cd20ca4
Daniel Kukula <daniel.kuku@gmail.com>
2018-04-18 00:35:21 +0100

    conversation and message tables

---
+++ app/models/conversation.rb
@@ -0,0 +1,9 @@
+class Conversation < ApplicationRecord
+  belongs_to :sender, :foreign_key => :sender_id, class_name: 'User'
+  belongs_to :task, :foreign_key => :task_id, class_name: 'Task'
+  has_many :messages, dependent: :destroy
+  validates_uniqueness_of :sender_id, :scope => :task_id
+  scope :between, -> (sender_id,task_id) do
+  where("(conversations.sender_id = ? AND conversations.task_id =?)", sender_id,task_id)
+ end
+end
